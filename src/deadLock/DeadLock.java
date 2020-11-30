da7a87d39fb042e5d725f720320bfbb6cdabcb06
liuxiaoyong <flxymichael@163.com>
2020-11-30 23:48:12 +0800

    解除死锁

--- src/deadLock/DeadLock.java
+++ src/deadLock/DeadLock.java
@@ -1,5 +1,13 @@
 package deadLock;
 
+//死锁发生的四个条件：
+//1.互斥：一个资源一次只能被一个进程（线程使用）
+//2.请求与保持：一个进程因为请求资源而被阻塞的时候，不会释放已经持有的资源
+//3.不剥夺：进程已经获得的资源，再未使用完成之前，不能强行剥夺。即2中的资源，既不会主动释放，也不能强行剥夺
+//4.循环等待：多干个进程形成一种头尾相接的循环等待资源关系。
+
+//只要破坏上述一个条件，就可避免死锁
+//在该例子中，通过破坏循环等待条件，解除了死锁。即，不能让一个人拿着一个资源一直等待其他资源
 public class DeadLock {
     public static void main(String[] args) {
         Makeup m1 = new Makeup("白雪公主",0);
@@ -40,22 +48,22 @@ class Makeup extends Thread{
         if (choice==0){
             synchronized (mirror){
                 System.out.println(this.name+"拿到了镜子");
-                //sleep可以放大问题的发生性
+                //sleep可以放大问题的发生性,不加sleep可能某一个线程跑很快直接把两个锁拿到就跑完了
                 Thread.sleep(1000);
-                synchronized (lipstick){
-                    System.out.println(this.name+"拿到了口红");
-                }
-                System.out.println(this.name+"化完妆了");
             }
+            synchronized (lipstick){
+                System.out.println(this.name+"拿到了口红");
+            }
+            System.out.println(this.name+"化完妆了");
         }else{
             synchronized (lipstick){
                 System.out.println(this.name+"拿到了口红");
-                synchronized (mirror){
-                    System.out.println(this.name+"拿到了镜子");
-                }
                 Thread.sleep(1000);
-                System.out.println(this.name+"化完妆了");
             }
+            synchronized (mirror){
+                System.out.println(this.name+"拿到了镜子");
+            }
+            System.out.println(this.name+"化完妆了");
         }
     }
 }

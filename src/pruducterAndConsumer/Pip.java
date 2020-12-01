ac8d943a802f0a41fbd4aef3f7aa6f477de039fa
liuxiaoyong <flxymichael@163.com>
2020-12-01 20:58:27 +0800

    解决了生产-消费的顺序问题，其实就是让消费者等了一会

--- src/pruducterAndConsumer/Pip.java
+++ src/pruducterAndConsumer/Pip.java
@@ -12,12 +12,12 @@ public class Pip {
         Producer producer01 = new Producer(productContainer);
         Customer customer01 = new Customer(productContainer);
 
-        new Thread(producer01,"producer01").start();
-        new Thread(producer01,"producer02").start();
-        new Thread(customer01,"customer01").start();
+        new Thread(producer01, "producer01").start();
+        new Thread(customer01, "customer01").start();
     }
 }
-class Producer implements Runnable{
+
+class Producer implements Runnable {
     private ProductContainer productContainer;
 
     public Producer(ProductContainer productContainer) {
@@ -27,17 +27,13 @@ class Producer implements Runnable{
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
-            try {
-                Thread.sleep(100);
-                productContainer.push(new Product(i));
-                System.out.println(Thread.currentThread().getName()+"生产了"+i+"号产品");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            productContainer.push(new Product(i));
+            System.out.println(Thread.currentThread().getName() + "生产了" + i + "号产品");
         }
     }
 }
-class Customer implements Runnable{
+
+class Customer implements Runnable {
     private ProductContainer productContainer;
 
     public Customer(ProductContainer productContainer) {
@@ -46,18 +42,19 @@ class Customer implements Runnable{
 
     @Override
     public void run() {
-        for (int i = 0; i <100 ; i++) {
+        for (int i = 0; i < 100; i++) {
+            Product product = productContainer.consume();
             try {
                 Thread.sleep(100);
-                Product product = productContainer.consume();
-                System.out.println(Thread.currentThread().getName()+"消费了"+product.id+"号产品");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            System.out.println(Thread.currentThread().getName() + "消费了---->" + product.id + "号产品");
         }
     }
 }
-class Product{
+
+class Product {
     int id;
 
     public Product(int id) {
@@ -65,11 +62,12 @@ class Product{
     }
 }
 
-class ProductContainer{
-    private int MAX_NUM =10;
+class ProductContainer {
+    private int MAX_NUM = 10;
     private int count = 0;
     Product[] products = new Product[MAX_NUM];
-    public synchronized void push(Product product) throws InterruptedException {
+
+    public synchronized void push(Product product) {
         //如果容器满了，通知消费者消费
 
         //多个线程阻塞于此，当被notifyAll时，第一个被唤醒的线程拿到count==MAX_NUM-1没问题，第二个就惨了.
@@ -78,20 +76,30 @@ class ProductContainer{
 //        }
 
         //改用while解决了数组越界的问题，但是现在多个生产者会生产同一个商品，为什么？
-          while (count>=MAX_NUM){
-              this.wait();
-          }
+        while (count >= MAX_NUM) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         //没满就继续生产
-            products[count]=product;
-            count++;
-            this.notifyAll();
+        products[count] = product;
+        count++;
+        this.notifyAll();
     }
 
-    public synchronized Product consume() throws InterruptedException {
+    public synchronized Product consume() {
         //容器为空则通知生产者生产
-        if (count==0){
-            this.wait();
+        while (count <= 0) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+
         //否则继续消费
         count--;
         Product product = products[count];
---
+++ src/pruducterAndConsumer/SignalLight.java
@@ -0,0 +1,5 @@
+package pruducterAndConsumer;
+//生产者消费者问题解决方法2：信号灯，标志位
+public class SignalLight {
+
+}
